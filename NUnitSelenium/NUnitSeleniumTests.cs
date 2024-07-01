f3513f1632771291475d812bc98fe9c6a693df13
Abid Kidwai <abidkidwai786@gmail.com>
2024-07-01 23:41:02 +0530

    first commit

--- NUnitSelenium/NUnitSeleniumTests.cs
+++ NUnitSelenium/NUnitSeleniumTests.cs
@@ -7,35 +7,30 @@ using System.Collections.Generic;
 
 namespace NUnitSelenium
 {
-    [TestFixture("chrome", "60", "Windows 10")]
-    [TestFixture("internet explorer", "11", "Windows 7")]
-    [TestFixture("firefox", "60", "Windows 7")]
-    [TestFixture("chrome", "71", "Windows 7")]
-    [TestFixture("internet explorer", "11", "Windows 10")]
-    [TestFixture("firefox", "58", "Windows 7")]
-    [TestFixture("chrome", "67", "Windows 7")]
-    [TestFixture("internet explorer", "10", "Windows 7")]
-    [TestFixture("firefox", "55", "Windows 7")]
+
+    [TestFixture("Pixel 3", "9", "android")]
+    [TestFixture("Galaxy S21 5G", "11", "android")]
+    [TestFixture("OnePlus 11", "13", "android")]
     [Parallelizable(ParallelScope.Children)]
     public class NUnitSeleniumSample
     {
-        public static string LT_USERNAME = Environment.GetEnvironmentVariable("LT_USERNAME") ==null ? "your username" : Environment.GetEnvironmentVariable("LT_USERNAME");
-        public static string LT_ACCESS_KEY = Environment.GetEnvironmentVariable("LT_ACCESS_KEY") == null ? "your accessKey" : Environment.GetEnvironmentVariable("LT_ACCESS_KEY");
+        public static string LT_USERNAME = "";
+        public static string LT_ACCESS_KEY = "";
         public static bool tunnel = Boolean.Parse(Environment.GetEnvironmentVariable("LT_TUNNEL")== null ? "false" : Environment.GetEnvironmentVariable("LT_TUNNEL"));       
         public static string build = Environment.GetEnvironmentVariable("LT_BUILD") == null ? "your build name" : Environment.GetEnvironmentVariable("LT_BUILD");
-        public static string seleniumUri = "https://hub.lambdatest.com:443/wd/hub";
+        public static string seleniumUri = "https://mobile-hub.lambdatest.com:443/wd/hub";
 
 
         ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
-        private String browser;
+        private String device;
         private String version;
-        private String os;
+        private String platform;
 
-        public NUnitSeleniumSample(String browser, String version, String os)
+        public NUnitSeleniumSample(String device, String version, String platform)
         {
-            this.browser = browser;
+            this.device = device;
             this.version = version;
-            this.os = os;
+            this.platform = platform;
         }
 
         [SetUp]
@@ -43,9 +38,11 @@ namespace NUnitSelenium
         {
             
             DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.SetCapability(CapabilityType.BrowserName, browser);
-            capabilities.SetCapability(CapabilityType.Version, version);
-            capabilities.SetCapability(CapabilityType.Platform, os);
+            capabilities.SetCapability("deviceName", device);
+            capabilities.SetCapability("platformVersion", version);
+            capabilities.SetCapability("platformName", platform);
+            capabilities.SetCapability("isRealMobile", true);
+            capabilities.SetCapability("app", "lt://proverbial-android");
 
             //Requires a named tunnel.
             if (tunnel)
@@ -54,7 +51,7 @@ namespace NUnitSelenium
             }
             if (build != null)
             {
-                capabilities.SetCapability("build", build);
+                capabilities.SetCapability("build", "C# NUNIT APPIUM PARALLEL");
             }
           
             capabilities.SetCapability("user", LT_USERNAME);
@@ -72,27 +69,20 @@ namespace NUnitSelenium
        public void Todotest()
         {
             {
-                Console.WriteLine("Navigating to todos app.");
-                driver.Value.Navigate().GoToUrl("https://lambdatest.github.io/sample-todo-app/");
 
-                driver.Value.FindElement(By.Name("li4")).Click();
-                Console.WriteLine("Clicking Checkbox");
-                driver.Value.FindElement(By.Name("li5")).Click();
+                driver.Value.FindElement(By.Id("color")).Click();
+                Thread.Sleep(3000);
+                driver.Value.FindElement(By.Id("color")).Click();
+                Thread.Sleep(3000);
+                Console.WriteLine("Clicking color");
+                driver.Value.FindElement(By.Id("Text")).Click();
+                Thread.Sleep(3000);
 
+                driver.Value.FindElement(By.Id("toast")).Click();
+                Thread.Sleep(3000);
+                driver.Value.FindElement(By.Id("notification")).Click();
+                Thread.Sleep(3000);
 
-                // If both clicks worked, then te following List should have length 2
-                IList<IWebElement> elems = driver.Value.FindElements(By.ClassName("done-true"));
-                // so we'll assert that this is correct.
-                Assert.AreEqual(2, elems.Count);
-
-                Console.WriteLine("Entering Text");
-                driver.Value.FindElement(By.Id("sampletodotext")).SendKeys("Yey, Let's add it to list");
-                driver.Value.FindElement(By.Id("addbutton")).Click();
-
-
-                // lets also assert that the new todo we added is in the list
-                string spanText = driver.Value.FindElement(By.XPath("/html/body/div/div/div/ul/li[6]/span")).Text;
-                Assert.AreEqual("Yey, Let's add it to list", spanText);
 
             }
         }
---
+++ NUnitSelenium/bin/Release/NUnitSeleniumTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace NUnitSelenium
+{
+
+    [TestFixture("Pixel 3", "9", "android")]
+    [TestFixture("Galaxy S21 5G", "11", "android")]
+    [TestFixture("OnePlus 11", "13", "android")]
+    [Parallelizable(ParallelScope.Children)]
+    public class NUnitSeleniumSample
+    {
+        public static string LT_USERNAME = "abidKidwai786";
+        public static string LT_ACCESS_KEY = "8WPqR8NybCKOmV7P5YXI8bbkNsrHBg0TURsREBa69FIRB54LzA";
+        public static bool tunnel = Boolean.Parse(Environment.GetEnvironmentVariable("LT_TUNNEL")== null ? "false" : Environment.GetEnvironmentVariable("LT_TUNNEL"));       
+        public static string build = Environment.GetEnvironmentVariable("LT_BUILD") == null ? "your build name" : Environment.GetEnvironmentVariable("LT_BUILD");
+        public static string seleniumUri = "https://mobile-hub.lambdatest.com:443/wd/hub";
+
+
+        ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
+        private String device;
+        private String version;
+        private String platform;
+
+        public NUnitSeleniumSample(String device, String version, String platform)
+        {
+            this.device = device;
+            this.version = version;
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.SetCapability("deviceName", device);
+            capabilities.SetCapability("platformVersion", version);
+            capabilities.SetCapability("platformName", platform);
+            capabilities.SetCapability("isRealMobile", true);
+            capabilities.SetCapability("app", "lt://proverbial-android");
+
+            //Requires a named tunnel.
+            if (tunnel)
+            {
+                capabilities.SetCapability("tunnel", tunnel);
+            }
+            if (build != null)
+            {
+                capabilities.SetCapability("build", "C# NUNIT APPIUM PARALLEL");
+            }
+          
+            capabilities.SetCapability("user", LT_USERNAME);
+            capabilities.SetCapability("accessKey", LT_ACCESS_KEY);
+
+            capabilities.SetCapability("name",
+            String.Format("{0}:{1}",
+            TestContext.CurrentContext.Test.ClassName,
+            TestContext.CurrentContext.Test.MethodName));
+            driver.Value = new RemoteWebDriver(new Uri(seleniumUri), capabilities, TimeSpan.FromSeconds(600));
+            Console.Out.WriteLine(driver);
+        }
+
+        [Test]
+       public void Todotest()
+        {
+            {
+
+                driver.Value.FindElement(By.Id("color")).Click();
+                Thread.Sleep(3000);
+                driver.Value.FindElement(By.Id("color")).Click();
+                Thread.Sleep(3000);
+                Console.WriteLine("Clicking color");
+                driver.Value.FindElement(By.Id("Text")).Click();
+                Thread.Sleep(3000);
+
+                driver.Value.FindElement(By.Id("toast")).Click();
+                Thread.Sleep(3000);
+                driver.Value.FindElement(By.Id("notification")).Click();
+                Thread.Sleep(3000);
+
+
+            }
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            bool passed = TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Passed;
+            try
+            {
+                // Logs the result to LambdaTest
+                ((IJavaScriptExecutor)driver.Value).ExecuteScript("lambda-status=" + (passed ? "passed" : "failed"));
+            }
+            finally
+            {
+                
+                // Terminates the remote webdriver session
+                driver.Value.Quit();
+            }
+        }
+    }
+}
